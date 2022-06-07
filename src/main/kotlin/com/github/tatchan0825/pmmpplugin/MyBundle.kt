816b37ebec7ec17ffb970de0568c681c38d10728
GitHub Action <action@github.com>
2022-06-07 12:33:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/tatchan0825/pmmpplugin/MyBundle.kt
+++ src/main/kotlin/com/github/tatchan0825/pmmpplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.tatchan0825.pmmpplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/tatchan0825/pmmpplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tatchan0825/pmmpplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tatchan0825.pmmpplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tatchan0825.pmmpplugin.MyBundle
 
 class MyApplicationService {
 
